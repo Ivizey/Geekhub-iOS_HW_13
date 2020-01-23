e3de1a580e699153ddfc110d2324573e50967d68
Pavel Bondar <ivizey@gmail.com>
2020-01-23 16:29:58 +0200

    Initial Commit

---
+++ home_work_13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  home_work_13
+//
+//  Created by Pavel Bondar on 23.01.2020.
+//  Copyright © 2020 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
